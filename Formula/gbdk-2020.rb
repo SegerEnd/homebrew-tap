c9fa0f5771348d4c30da808401b775786302b19a
SegerEnd <gj.endendijk@gmail.com>
2026-04-07 13:52:50 +0200

    ci: add test-bot workflow with ROM smoke build

--- Formula/gbdk-2020.rb
+++ Formula/gbdk-2020.rb
@@ -1,6 +1,7 @@
 class Gbdk2020 < Formula
   desc "Cross-platform development kit for Game Boy, NES, SMS, and Game Gear"
   homepage "https://github.com/gbdk-2020/gbdk-2020"
+  version "4.5.0"
   license "GPL-2.0-only"
 
   livecheck do
@@ -8,19 +9,23 @@ class Gbdk2020 < Formula
     strategy :github_latest
   end
 
-  if OS.mac?
-    if Hardware::CPU.arm?
+  on_macos do
+    on_arm do
       url "https://github.com/gbdk-2020/gbdk-2020/releases/download/4.5.0/gbdk-macos-arm64.tar.gz"
       sha256 "289ee60e46c5a2785a21e35533f84a5131ed4a063b21b0dbdedc9a10af15bf78"
-    else
+    end
+    on_intel do
       url "https://github.com/gbdk-2020/gbdk-2020/releases/download/4.5.0/gbdk-macos.tar.gz"
       sha256 "1aa549d12032d8f6509d11923bb28b1a453098f42597feb378e9a42541f8fd89"
     end
-  elsif OS.linux?
-    if Hardware::CPU.arm?
+  end
+
+  on_linux do
+    on_arm do
       url "https://github.com/gbdk-2020/gbdk-2020/releases/download/4.5.0/gbdk-linux-arm64.tar.gz"
       sha256 "31eb2235f0fdb60163d0b1e9574a022098d6069cd56606a1daca4478a46e0439"
-    else
+    end
+    on_intel do
       url "https://github.com/gbdk-2020/gbdk-2020/releases/download/4.5.0/gbdk-linux64.tar.gz"
       sha256 "d7857a5f6d135ee4c249043ca26aad9f2ec8ab5d4106d97720d404114f42605c"
     end
@@ -51,5 +56,9 @@ class Gbdk2020 < Formula
 
   test do
     assert_match "lcc", shell_output("#{bin}/lcc 2>&1", 1)
+
+    cp_r "#{opt_libexec}/examples/gb/template_minimal/.", testpath
+    system "make"
+    assert_predicate Dir["#{testpath}/*.gb"].first, :present?, "no .gb ROM produced"
   end
 end
