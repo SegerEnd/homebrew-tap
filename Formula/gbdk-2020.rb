01442899c7135bee66886fb7a1820dd37645cd85
SegerEnd <gj.endendijk@gmail.com>
2026-04-07 13:42:06 +0200

    gbdk-2020: fix audit, add arm64, add auto-bump workflow

--- Formula/gbdk-2020.rb
+++ Formula/gbdk-2020.rb
@@ -1,7 +1,6 @@
 class Gbdk2020 < Formula
   desc "Cross-platform development kit for Game Boy, NES, SMS, and Game Gear"
   homepage "https://github.com/gbdk-2020/gbdk-2020"
-  version "4.5.0"
   license "GPL-2.0-only"
 
   livecheck do
@@ -9,14 +8,22 @@ class Gbdk2020 < Formula
     strategy :github_latest
   end
 
-  on_macos do
-    url "https://github.com/gbdk-2020/gbdk-2020/releases/download/#{version}/gbdk-#{version}-macos.tar.gz"
-    sha256 "REPLACE_MACOS_SHA256"
-  end
-
-  on_linux do
-    url "https://github.com/gbdk-2020/gbdk-2020/releases/download/#{version}/gbdk-#{version}-linux64.tar.gz"
-    sha256 "REPLACE_LINUX_SHA256"
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/gbdk-2020/gbdk-2020/releases/download/4.5.0/gbdk-macos-arm64.tar.gz"
+      sha256 "289ee60e46c5a2785a21e35533f84a5131ed4a063b21b0dbdedc9a10af15bf78"
+    else
+      url "https://github.com/gbdk-2020/gbdk-2020/releases/download/4.5.0/gbdk-macos.tar.gz"
+      sha256 "1aa549d12032d8f6509d11923bb28b1a453098f42597feb378e9a42541f8fd89"
+    end
+  elsif OS.linux?
+    if Hardware::CPU.arm?
+      url "https://github.com/gbdk-2020/gbdk-2020/releases/download/4.5.0/gbdk-linux-arm64.tar.gz"
+      sha256 "31eb2235f0fdb60163d0b1e9574a022098d6069cd56606a1daca4478a46e0439"
+    else
+      url "https://github.com/gbdk-2020/gbdk-2020/releases/download/4.5.0/gbdk-linux64.tar.gz"
+      sha256 "d7857a5f6d135ee4c249043ca26aad9f2ec8ab5d4106d97720d404114f42605c"
+    end
   end
 
   def install
@@ -28,7 +35,9 @@ class Gbdk2020 < Formula
 
     # Patch template Makefiles to use the Homebrew install path
     Dir["#{libexec}/examples/**/Makefile"].each do |mf|
-      inreplace mf, /^(\s*GBDK\s*=\s*).*$/, "\\1#{opt_libexec}/" rescue nil
+      inreplace mf, /^(\s*GBDK\s*=\s*).*$/, "\\1#{opt_libexec}/"
+    rescue Utils::InreplaceError
+      next
     end
   end
 
