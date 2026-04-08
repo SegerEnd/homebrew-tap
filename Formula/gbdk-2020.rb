2679d095e6d2b75562c01f890d2d32e3e5c5f23d
SegerEnd <76704606+SegerEnd@users.noreply.github.com>
2026-04-08 07:20:41 +0000

    gbdk-2020: 4.5.0 -> 4.5.0

--- Formula/gbdk-2020.rb
+++ Formula/gbdk-2020.rb
@@ -11,22 +11,22 @@ class Gbdk2020 < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/gbdk-2020/gbdk-2020/releases/download/#{version}/gbdk-macos-arm64.tar.gz"
+      url "https://github.com/gbdk-2020/gbdk-2020/releases/download/4.5.0/gbdk-macos-arm64.tar.gz"
       sha256 "289ee60e46c5a2785a21e35533f84a5131ed4a063b21b0dbdedc9a10af15bf78"
     end
     on_intel do
-      url "https://github.com/gbdk-2020/gbdk-2020/releases/download/#{version}/gbdk-macos.tar.gz"
+      url "https://github.com/gbdk-2020/gbdk-2020/releases/download/4.5.0/gbdk-macos.tar.gz"
       sha256 "1aa549d12032d8f6509d11923bb28b1a453098f42597feb378e9a42541f8fd89"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/gbdk-2020/gbdk-2020/releases/download/#{version}/gbdk-linux-arm64.tar.gz"
+      url "https://github.com/gbdk-2020/gbdk-2020/releases/download/4.5.0/gbdk-linux-arm64.tar.gz"
       sha256 "31eb2235f0fdb60163d0b1e9574a022098d6069cd56606a1daca4478a46e0439"
     end
     on_intel do
-      url "https://github.com/gbdk-2020/gbdk-2020/releases/download/#{version}/gbdk-linux64.tar.gz"
+      url "https://github.com/gbdk-2020/gbdk-2020/releases/download/4.5.0/gbdk-linux64.tar.gz"
       sha256 "d7857a5f6d135ee4c249043ca26aad9f2ec8ab5d4106d97720d404114f42605c"
     end
   end
